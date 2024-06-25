90e29177b7d67ce16bccbd5d2b58196dc9716657
Strokeraa <luisgpereiraa@gmail.com>
2024-06-25 15:32:02 -0300

    att

--- arquivos/teste.py
+++ arquivos/teste.py
@@ -14,14 +14,17 @@ output_dir = 'C:\\Users\\cebe_lgopereira\\OneDrive - SABO INDUSTRIA E COMERCIO D
 for sheet_name in xls.sheet_names:
     # Ler cada planilha
     df = pd.read_excel(xls, sheet_name=sheet_name)
-    
+
+    # Remover linhas vazias
+    df.dropna(how='all', inplace=True) 
+
     # Criar um nome de arquivo seguro para o sistema de arquivos
     safe_sheet_name = "".join([c if c.isalnum() else "_" for c in sheet_name])
-    
+
     # Definir o caminho de saída para cada planilha
     output_file = os.path.join(output_dir, f'{safe_sheet_name}.txt')
-    
+
     # Salvar a planilha como um arquivo TXT separado por vírgulas
     df.to_csv(output_file, sep=',', index=False)
-    
+
     print(f'Salvo: {output_file}')
